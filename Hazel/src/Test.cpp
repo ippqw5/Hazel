9447050ab3169d3d0fef8c69fcbd02f90afaa84b
ippqw5 <ippqw5@163.com>
2023-01-13 17:05:34 +0800

    P13 Layers

--- Hazel/src/Test.cpp
+++
@@ -1,6 +0,0 @@
-#include "Test.h"
-
-void Hazel::Print()
-{
-	std::cout << "Welcome Hazel Engine" << std::endl;
-}
--- Hazel/src/Test.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-namespace Hazel {
-
-	__declspec(dllexport) void Print();
-}
