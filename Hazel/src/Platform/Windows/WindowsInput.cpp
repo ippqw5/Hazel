b7e7634bb44e73306c4a314cd522edc8dc4d5284
ippqw5 <ippqw5@163.com>
2023-01-16 20:14:31 +0800

    .

---
+++ Hazel/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,31 @@
+#include "hzpch.h"
+#include "WindowsInput.h"
+#include "hazel\Application.h"
+
+#include <GLFW\glfw3.h>
+namespace Hazel {
+
+	Input* WindowsInput::s_Instance = new WindowsInput();
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+
+		return state == GLFW_PRESS ;
+	}
+	std::pair<float,float> WindowsInput::GetMousePosImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return { float(xpos), float(ypos) };
+	}
+
+}
---
+++ Hazel/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "hazel/Input.h"
+
+namespace Hazel {
+	
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float,float> GetMousePosImpl() override;
+
+	};
+}
